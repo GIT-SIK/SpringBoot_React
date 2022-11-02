0fa09f3d1e1d431e4142c1c62c7e160f98986c65
GIT-SIK <xeno_s@naver.com>
2022-11-02 16:02:26 +0900

    Mybatis

--- src/main/java/dev/scheincomp/mybatis/controller/IndexController.java
+++ src/main/java/dev/scheincomp/mybatis/controller/IndexController.java
@@ -4,7 +4,7 @@ import dev.scheincomp.mybatis.dao.IndexDao;
 import dev.scheincomp.mybatis.vo.IndexEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,11 +17,11 @@ public class IndexController {
     private IndexDao indexDao;
 
 
-    @RequestMapping("")
-    public void index(){
+    @GetMapping (value = "/")
+    public String MybatisValue(){
       List<IndexEntity> listIndex = indexDao.listIndex();
-
       System.out.println(Arrays.asList(listIndex));
-
+      return "/index.html";
     }
+
 }
